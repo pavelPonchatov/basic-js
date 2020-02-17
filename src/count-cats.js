c252101603d7b8425fc8aefa62bda61507ba8686
pavel <pavel.pronchatov155@gmail.com>
2020-02-17 23:39:58 +0400

    first question

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,16 @@
-module.exports = function countCats(/* matrix */) {
+module.exports = function countCats(arr) {
+
+  let res = 0;
+
+  for(let i = 0; i < arr.length;i++){
+    if(arr[i] == "^^"){
+      res += 1;
+    }
+  }
+
+  return res;
+
+
   throw 'Not implemented';
-  // remove line with error and write your code here
+  
 };
