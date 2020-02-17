84c27e369cd0b847b923bce718a1186963feb73a
pavel <pavel.pronchatov155@gmail.com>
2020-02-18 01:15:00 +0400

    fix

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,7 +2,7 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if(sampleActivity != undefined && typeof sampleActivity == "string" && sampleActivity > 0){
+  if(sampleActivity != undefined && typeof sampleActivity == "string" && MODERN_ACTIVITY > sampleActivity > 0){
     let k = 0.693 / HALF_LIFE_PERIOD;
     let t = Math.ceil(Math.log((MODERN_ACTIVITY / sampleActivity) / k));
 
