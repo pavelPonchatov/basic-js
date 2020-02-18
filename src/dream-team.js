6f0834bfb1ae3448fdfcc6be67383dd192975677
pavel <pavel.pronchatov155@gmail.com>
2020-02-19 01:03:30 +0400

    feat: Dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,23 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let res = [];
+  if(Array.isArray(members) == false){
+    return false;
+  }
+
+  for(let i = 0; i < members.length; i++){
+    if(typeof members[i] == "number" || typeof members[i] == "boolean" ||  typeof members[i] == "undefined"){
+      members.splice(i,1);
+    }
+  }
+
+  for(let i = 0; i < members.length; i++){
+    if(typeof members[i] == "string"){
+      let name = members[i].replace(/\s+/g, '').split("");
+      res.push(name[0]);
+      
+    }
+
+  }
+
+  return res.sort().join("").toUpperCase();
 };
