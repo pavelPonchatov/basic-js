1afdc18a0d6174cf769c0e77b1f52b4b5c9d12fb
pavel <pavel.pronchatov155@gmail.com>
2020-02-20 01:25:59 +0400

    feat: fix count-cat

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,18 +1,12 @@
 module.exports = function countCats(arr) {
-
   let res = 0;
-
   for(let i = 0; i < arr.length;i++){
-    for(let j = 0; i < arr[i].length;j++){
+    for(let j = 0; j < arr[i].length;j++){
       if(arr[i][j] == "^^"){
         res += 1;
       }
     }
 
   }
-
   return res;
-
-
-  
 };
