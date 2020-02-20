f999ae373a81f3a7ba9af355afc5f4213000a7f7
pavel <pavel.pronchatov155@gmail.com>
2020-02-20 23:47:46 +0400

    feat: transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,29 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    try{
+        if(Array.isArray(arr) != true){
+            gggg();
+        }   
+    }catch(err){
+        throw 'Not implemented';
+    }
+
+    for(let i = 0; i < arr.length; i++){
+       
+        if(arr[i] == "--double-next"){
+            arr[i] = arr[i+1];
+        }else if(arr[i] == "--discard-prev"){
+            arr.splice(i,1);
+            arr.splice(i-1,1);
+            
+        }else if(arr[i] == "--double-prev"){
+            arr[i] = arr[i-1];
+            
+        }else if(arr[i] == "--discard-next"){
+            arr.splice(i,1);
+            arr.splice(i+1,1);
+            
+        }
+    }
+
+    return arr;
 };
