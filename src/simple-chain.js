64b8ad1a92f497989d42ecd4ee61cc103ee70b11
pavel <pavel.pronchatov155@gmail.com>
2020-03-01 00:56:06 +0400

    feat: add simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,41 @@
 const chainMaker = {
+  arrChain: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.arrChain.length;;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let link = `( ${value} )`;
+    if(value == null){
+      link = "( null )";
+    }else if(value == undefined){
+      link = "( )";
+    }
+    this.arrChain.push(link);
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if(!Number(position) || position > this.arrChain.length || position < 1 || position == 0){
+      this.arrChain = [];
+      throw new Error();
+    }
+    this.arrChain.splice(position-1,1);
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+   this.arrChain.reverse();
+   return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let allChain = this.arrChain.slice(0, this.arrChain.length).join('~~');
+    console.log(allChain);
+    this.arrChain = [];
+    return allChain;
   }
 };
 
 module.exports = chainMaker;
+
+
+
+
+
