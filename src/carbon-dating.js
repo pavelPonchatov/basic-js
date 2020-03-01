c64583ab18b6cc2a545e82fa91c77cb9605f7e4f
pavel <pavel.pronchatov155@gmail.com>
2020-03-02 00:18:09 +0400

    feat: add what-season

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,17 +1,19 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(sampleActivity) {
-  if(sampleActivity != undefined && typeof sampleActivity == "string" && MODERN_ACTIVITY > sampleActivity > 0){
-    let k = 0.693 / HALF_LIFE_PERIOD;
-    let t = Math.ceil(Math.log((MODERN_ACTIVITY / sampleActivity) / k));
+ module.exports = function dateSample(sampleActivity) {
+  if(sampleActivity == 0 || sampleActivity < 0 || sampleActivity == 9000){
+    return false;
+  }
 
-    return t;
+  if(sampleActivity != undefined && typeof sampleActivity == "string" && MODERN_ACTIVITY > sampleActivity > 0){
+    return Math.ceil(Math.log(MODERN_ACTIVITY / Number.parseFloat(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
   }else{
     return false;
   }
 
 
+
  
 
-};
+}
