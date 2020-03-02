bc5e60b062935523c300025575b6d3a6c2ef376e
pavel <pavel.pronchatov155@gmail.com>
2020-03-03 02:14:55 +0400

    feat: add extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,55 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+ module.exports = function repeater(str, options) {
+    let arrStr = [];
+    if(options.repeatTimes == undefined){
+        arrStr.push(str + options.addition);
+    }
+    for(let i = 0; i < options.repeatTimes; i++){
+        if(Object.keys(options).length < 3){
+            arrStr.push(str);
+        }else if("additionSeparator" in options == false){
+            arrStr.push(str);
+            arrStr[i]= (str + (options.addition ).repeat(options.additionRepeatTimes) );
+            console.log(1);
+        }else{
+            arrStr.push(str);
+            arrStr[i]= (str + (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes) );
+            arrStr[i] = arrStr[i].slice(0,arrStr[i].length - options.additionSeparator.length);
+        }
+    }
+    if(options.separator == undefined){
+        return arrStr.join("+");
+    }
+
+    return arrStr.join(options.separator);
 };
   
+
+// function repeater(str, options) {
+//     let arrStr = [];
+//     if(options.repeatTimes == undefined){
+//         arrStr.push(str + options.addition);
+//     }
+//     for(let i = 0; i < options.repeatTimes; i++){
+//         if(Object.keys(options).length < 3){
+//             arrStr.push(str);
+//         }else if("additionSeparator" in options == false){
+//             arrStr.push(str);
+//             arrStr[i]= (str + (options.addition ).repeat(options.additionRepeatTimes) );
+//             console.log(1);
+//         }else{
+//             arrStr.push(str);
+//             arrStr[i]= (str + (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes) );
+//             arrStr[i] = arrStr[i].slice(0,arrStr[i].length - options.additionSeparator.length);
+//         }
+//     }
+//     if(options.separator == undefined){
+//         return arrStr.join("+");
+//     }
+
+//     return arrStr.join(options.separator);
+// };
+
+
+
+
+
